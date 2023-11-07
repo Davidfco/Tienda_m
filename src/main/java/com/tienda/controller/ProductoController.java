f1ff9a3802ac27e747b17df5890618b9e41bdf34
David <drayz@DESKTOP-9IMPJ1S>
2023-11-06 22:10:30 -0600

    Practica #4 CRUD Actualizar Producto

--- src/main/java/com/tienda/controller/ProductoController.java
+++ src/main/java/com/tienda/controller/ProductoController.java
@@ -1,9 +1,11 @@
 package com.tienda.controller;
 
+import com.tienda.domain.Categoria;
 import com.tienda.domain.Producto;
 import com.tienda.service.CategoriaService;
 import com.tienda.service.ProductoService;
 import com.tienda.service.impl.FirebaseStorageServiceImpl;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -28,7 +30,7 @@ public class ProductoController {
         var productos = productoService.getProductos(false);
         model.addAttribute("productos", productos);
         model.addAttribute("totalProductos", productos.size());
-        var categorias = categoriaService.getCategorias(false);
+        var categorias = categoriaService.getCategorias(true);
         model.addAttribute("categorias", categorias);
         return "/producto/listado";
     }
@@ -65,7 +67,9 @@ public class ProductoController {
     @GetMapping("/modificar/{idProducto}")
     public String productoModificar(Producto producto, Model model) {
         producto = productoService.getProducto(producto);
+        List<Categoria> categorias = categoriaService.getCategorias(true);
         model.addAttribute("producto", producto);
+        model.addAttribute("categorias", categorias);
         return "/producto/modifica";
     }
 }
